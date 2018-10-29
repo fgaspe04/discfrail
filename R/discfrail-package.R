394a9e8640596d3b953e79754f7316100e720c00
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2018-10-29 17:43:52 +0000

    Renamed simulation functions and documented datasets

--- R/discfrail-package.R
+++ R/discfrail-package.R
@@ -1,6 +1,6 @@
 ##' discfrail: Cox models for time-to-event data with nonparametric discrete group-specific frailties.
 ##'
-##' discfrail: Functions for fitting Cox proportional hazards models for grouped time-to-event data, where the shared group-specific frailties have a discrete nonparametric distribution. Groups of individuals are clustered into a number of latent "populations" of groups, each with a common frailty that is assumed to act multiplicatively on their hazard.   Covariates can be included in the manner of the standard Cox model.   The baseline hazard is left unspecified as in the Cox model, and estimation is performed by maximum partial likelihood through an EM algorithm. 
+##' discfrail: Functions for fitting Cox proportional hazards models for grouped time-to-event data, where the shared group-specific frailties have a discrete nonparametric distribution. Groups of individuals are clustered into a number of latent "populations" of groups, each with a common frailty that is assumed to act multiplicatively on their hazard.   Covariates can be included through proportional hazards, in the manner of the standard Cox model.   The baseline hazard is left unspecified as in the Cox model, and estimation is performed by maximum partial likelihood through an EM algorithm. 
 ##'
 ##' There are also functions for simulating from these models, and from similar models with a parametric baseline survival function.
 ##'
@@ -12,7 +12,9 @@
 ##'
 ##' \code{\link{plot.npdf}} plots fitted survival or cumulative hazard curves from the fitted models, by group, coloured by the latent population the group belongs to. 
 ##'
-##' \code{\link{sim.npdf}} TODO Describe and rename
+##' \code{\link{sim_npdf}} simulates from a proportional hazards model with a flexible baseline survival distribution and discrete shared frailty terms.
+##'
+##' \code{\link{sim_weibdf}} simulates from a Weibull survival model with shared discrete frailty terms. 
 ##'
 ##' 
 ##' @name discfrail-package
